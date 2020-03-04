ae8ad3dbe86562577aabfabd878e023343316e77
Mikeyouf <mik_anarchy@hotmail.com>
2020-03-04 10:31:23 +0100

    modif css admin page

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -6,6 +6,7 @@ import { withAuthorization, withEmailVerification } from '../components/session/
 import { withFirebase } from '../../src/firebase/index';
 import { Switch, Route, Link } from 'react-router-dom';
 import Layout from '../components/Layout'
+import { TiArrowBackOutline } from "react-icons/ti";
 
 import styled from 'styled-components'
 import { pxToRem, colors, fonts } from '../theme/Helpers'
@@ -20,12 +21,12 @@ const Wrapper = styled.article`
 
 const Users = styled.article`
   width: ${pxToRem(600)};
-  border: 1px solid ${colors.accent};
   border-radius: 4px;
   padding: ${pxToRem(16)} ${pxToRem(8)};
   display: flex;
   flex-direction: column;
   align-items: center;
+  box-shadow: 0 0 3px 3px ${colors.accentRGBA};
 
   h2 {
     color: ${colors.accentLight};
@@ -101,10 +102,10 @@ const UserListBase = ({ firebase, ...props }) => {
       <h2>Utilisateurs</h2>
       {loading && <div>Loading ...</div>}
       <Liste>
-        {users.map(user => (
+        {users.map((user, i) => (
           <li key={user.uid}>
             <p>
-              Nom
+              {i + 1}
             </p>
             <p>
               {user.username}
@@ -121,10 +122,106 @@ const UserListBase = ({ firebase, ...props }) => {
   );
 }
 
+const User = styled.div`
+  width: 100%;
+`
+
+const Ligne = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: ${pxToRem(8)};
+  font-family: ${fonts.fontTexte};
+
+  &:nth-child(odd) {
+    background: ${colors.accentRGBA};
+  }
+
+  p {
+
+    &:nth-child(1) {
+      font-weight: bold;
+    }
+  }
+`
+
+const RolesWrapper = styled.div`
+  margin-top: ${pxToRem(16)};
+  padding: ${pxToRem(8)};
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid ${colors.accentRGBA};
+
+  div {
+    width: ${pxToRem(180)};
+    margin: ${pxToRem(8)} 0;
+    text-align: center;
+
+    &:nth-child(4) {
+      width: ${pxToRem(260)};
+    }
+
+    input {
+      margin-left: ${pxToRem(8)};
+    }
+  }
+`
+
+const Button = styled.button`
+  border: 1px solid ${colors.accent};
+  border-radius: 4px;
+  color: ${colors.accent};
+  cursor: pointer;
+  padding: ${pxToRem(8)} ${pxToRem(16)};
+  transition: all 0.3s ease-in;
+  font-size: ${pxToRem(18)};
+  box-shadow: 3px 3px 3px 1px ${colors.accentRGBA};
+
+  &:hover {
+    background: ${colors.accent};
+    color: ${colors.white};
+    transition: all 0.3s ease-in;
+    border: 1px solid ${colors.accentLight};
+  }
+`
+
+const Back = styled.div`
+  padding: ${pxToRem(8)};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    color: ${colors.accent};
+    border: 1px solid ${colors.accent};
+    border-radius: 4px;
+    padding: ${pxToRem(8)} ${pxToRem(16)};
+    transition: all 0.3s ease-in;
+    font-size: ${pxToRem(18)};
+    box-shadow: 3px 3px 3px 1px ${colors.accentRGBA};
+
+    &:hover {
+      background: ${colors.accent};
+      color: ${colors.white};
+      transition: all 0.3s ease-in;
+      border: 1px solid ${colors.accentLight};
+    }
+
+    span {
+      margin-left: ${pxToRem(8)};
+    }
+  }
+
+`
+
 const UserItemBase = ({ firebase, ...props }) => {
   const [ loading, setLoading ] = useState(false)
   const [ user, setUser ] = useState(null)
   const [ authUser, setAuthUser ] = useState('')
+  const [ roleUser, setRoleUser ] = useState([])
   const match = props.match.params.id
   
   useEffect(() => {
@@ -151,6 +248,15 @@ const UserItemBase = ({ firebase, ...props }) => {
     userRef.update({ ...user })
   }, [user, match, firebase.db])
 
+  useEffect(() => {
+    if(user) {
+      const role = [];
+      Object.keys(user.roles).map(item => item && role.push(user.roles[item]));
+      const newRole = role.filter(elt => elt !== "");
+      setRoleUser(newRole);
+    }
+  }, [user])
+
   const onSendPasswordResetEmail = () => {
     firebase.doPasswordReset(user.email);
   };
@@ -167,81 +273,84 @@ const UserItemBase = ({ firebase, ...props }) => {
     } else {
       const values = user.roles
       const roles = { ...values, [event.target.name] : '' }
-      await setUser({
-        roles: roles
-      })
+      await setUser(prevValues => ({
+        ...prevValues,
+        roles : roles
+      }))
     }
   };
 
   return (
-    <div>
-      <h2>Utilisateurs ({authUser || props.match.params.id})</h2>
+    <Users>
+      <h2>Utilisateur ({authUser || props.match.params.id})</h2>
       {loading && <div>Loading ...</div>}
       {user && (
-        <div>
-          <p>
-            <strong>ID:</strong> {user.id}
-          </p>
-          <p>
-            <strong>E-Mail:</strong> {user.email}
-          </p>
-          <p>
-            <strong>Nom:</strong> {user.username}
-          </p>
-          <div>
-            <strong>Rôle(s):</strong> {Object.keys(user.roles).map((role,i) => (<p key={i}>{user.roles[role]}</p>))}
-          </div>
-          <div>
-            <label>
-              Admin?
+        <User>
+          <Ligne>
+            <p>ID:</p>
+            <p>{user.id}</p>
+          </Ligne>
+          <Ligne>
+            <p>E-Mail:</p>
+            <p>{user.email}</p>
+          </Ligne>
+          <Ligne>
+            <p>Nom:</p>
+            <p>{user.username}</p>
+          </Ligne>
+          <Ligne>
+            <p>Rôle{roleUser.length > 1 ? 's' : ''}:</p>
+            {Object.keys(user.roles).map((role,i) => (
+            <p key={i}>{user.roles[role]}</p>
+            ))}
+          </Ligne>
+          <RolesWrapper>
+            <div>
+              <label htmlFor="admin">Admin ?</label>
               <input
                 name="ADMIN"
+                id="admin"
                 type="checkbox"
                 checked={user.roles.ADMIN ? true : false}
                 onChange={e => onChangeCheckbox(e, 'ADMIN')}
               />
-            </label>
-          </div>
-          <div>
-            <label>
-              Reader?
+            </div>
+            <div>
+              <label htmlFor="reader">Reader ?</label>
               <input
                 name="READER"
+                id="reader"
                 type="checkbox"
                 checked={user.roles.READER ? true : false}
                 onChange={e => onChangeCheckbox(e, 'READER')}
               />
-            </label>
-          </div>
-          <div>
-            <label>
-              EDITOR?
+            </div>
+            <div>
+              <label htmlFor="editor">Editor ?</label>
               <input
                 name="EDITOR"
+                id="editor"
                 type="checkbox"
                 checked={user.roles.EDITOR ? true : false}
                 onChange={e => onChangeCheckbox(e, 'EDITOR')}
               />
-            </label>
-          </div>
-          <p>
-            <button
+            </div>
+          </RolesWrapper>
+          <Back>
+            <Button
               type="button"
               onClick={() => onSendPasswordResetEmail()}
             >
               Envoyer un email de reset
-            </button>
-          </p>
-          <div>
-            <span>
-              <Link to={ROUTES.ADMIN}>
-                Retour
-              </Link>
-            </span>
-          </div>
-        </div>
+            </Button>
+            <Link to={ROUTES.ADMIN}>
+              <TiArrowBackOutline/>
+              <span>Retour</span>
+            </Link>
+          </Back>
+        </User>
       )}
-    </div>
+    </Users>
   );
 }
 
